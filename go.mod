1995ce01caf0cffd4487b29c7af13a5fed42d4a1
ChenHaoyang <63438478+C1en-QwQ@users.noreply.github.com>
2023-01-09 07:10:10 +0800

    RT
    RT

---
+++ go.mod
@@ -0,0 +1,93 @@
+module github.com/FloatTech/ZeroBot-Plugin
+
+go 1.19
+
+require (
+	github.com/Baidu-AIP/golang-sdk v1.1.1
+	github.com/Coloured-glaze/gg v1.3.4
+	github.com/FloatTech/AnimeAPI v1.6.1-0.20230107074849-392b0b10b149
+	github.com/FloatTech/floatbox v0.0.0-20230107061309-dcd5c0012dd3
+	github.com/FloatTech/sqlite v1.5.7
+	github.com/FloatTech/ttl v0.0.0-20220715042055-15612be72f5b
+	github.com/FloatTech/zbpctrl v1.5.3-0.20221210051848-740ab7bd6ec3
+	github.com/FloatTech/zbputils v1.6.1-0.20230107061700-dadcf8500e7a
+	github.com/RomiChan/syncx v0.0.0-20221202055724-5f842c53020e
+	github.com/RomiChan/websocket v1.4.3-0.20220227141055-9b2c6168c9c5
+	github.com/antchfx/htmlquery v1.2.5
+	github.com/corona10/goimagehash v1.1.0
+	github.com/fumiama/ahsai v0.1.0
+	github.com/fumiama/cron v1.3.0
+	github.com/fumiama/go-base16384 v1.6.1
+	github.com/fumiama/go-registry v0.2.5-0.20221121111817-44b0846bdce6
+	github.com/fumiama/gotracemoe v0.0.3
+	github.com/fumiama/jieba v0.0.0-20221203025406-36c17a10b565
+	github.com/fumiama/unibase2n v0.0.0-20221020155353-02876e777430
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jozsefsallai/gophersauce v1.0.1
+	github.com/lucas-clemente/quic-go v0.31.1
+	github.com/mroth/weightedrand v1.0.0
+	github.com/pkg/errors v0.9.1
+	github.com/shirou/gopsutil/v3 v3.22.11
+	github.com/sirupsen/logrus v1.9.0
+	github.com/tidwall/gjson v1.14.4
+	github.com/wcharczuk/go-chart/v2 v2.1.0
+	github.com/wdvxdr1123/ZeroBot v1.6.6
+	gitlab.com/gomidi/midi/v2 v2.0.25
+	golang.org/x/image v0.3.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/FloatTech/rendercard v0.0.3 // indirect
+	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/ericpauley/go-quantize v0.0.0-20200331213906-ae555eb2afa4 // indirect
+	github.com/faiface/beep v1.1.0 // indirect
+	github.com/fumiama/go-simple-protobuf v0.1.0 // indirect
+	github.com/fumiama/gofastTEA v0.0.10 // indirect
+	github.com/gabriel-vasile/mimetype v1.0.4 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.1 // indirect
+	github.com/jfreymuth/vorbis v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/marten-seemann/qpack v0.3.0 // indirect
+	github.com/marten-seemann/qtls-go1-18 v0.1.3 // indirect
+	github.com/marten-seemann/qtls-go1-19 v0.1.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/onsi/ginkgo/v2 v2.2.0 // indirect
+	github.com/pkumza/numcn v1.0.0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	golang.org/x/exp/shiny v0.0.0-20221126150942-6ab00d035af9 // indirect
+	golang.org/x/mobile v0.0.0-20201217150744-e6ae53a27f4f // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	modernc.org/libc v1.21.5 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.4.0 // indirect
+	modernc.org/sqlite v1.20.0 // indirect
+)
+
+replace modernc.org/sqlite => github.com/fumiama/sqlite3 v1.20.0-with-win386
+
+replace github.com/remyoudompheng/bigfft => github.com/fumiama/bigfft v0.0.0-20211011143303-6e0bfa3c836b
---
+++ main.go
@@ -0,0 +1,303 @@
+// Package main ZeroBot-Plugin main file
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/FloatTech/ZeroBot-Plugin/kanban" // 在最前打印 banner
+
+	// ---------以下插件均可通过前面加 // 注释，注释后停用并不加载插件--------- //
+	// ----------------------插件优先级按顺序从高到低---------------------- //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	// ----------------------------高优先级区---------------------------- //
+	// vvvvvvvvvvvvvvvvvvvvvvvvvvvv高优先级区vvvvvvvvvvvvvvvvvvvvvvvvvvvv //
+	//               vvvvvvvvvvvvvv高优先级区vvvvvvvvvvvvvv               //
+	//                      vvvvvvv高优先级区vvvvvvv                      //
+	//                          vvvvvvvvvvvvvv                          //
+	//                               vvvv                               //
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/antiabuse" // 违禁词
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chat" // 基础词库
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/sleep_manage" // 统计睡眠时间
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/atri" // ATRI词库
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/manager" // 群管
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/thesaurus" // 词典匹配回复
+
+	_ "github.com/FloatTech/zbputils/job" // 定时指令触发器
+
+	//                               ^^^^                               //
+	//                          ^^^^^^^^^^^^^^                          //
+	//                      ^^^^^^^高优先级区^^^^^^^                      //
+	//               ^^^^^^^^^^^^^^高优先级区^^^^^^^^^^^^^^               //
+	// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^高优先级区^^^^^^^^^^^^^^^^^^^^^^^^^^^^ //
+	// ----------------------------高优先级区---------------------------- //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	// ----------------------------中优先级区---------------------------- //
+	// vvvvvvvvvvvvvvvvvvvvvvvvvvvv中优先级区vvvvvvvvvvvvvvvvvvvvvvvvvvvv //
+	//               vvvvvvvvvvvvvv中优先级区vvvvvvvvvvvvvv               //
+	//                      vvvvvvv中优先级区vvvvvvv                      //
+	//                          vvvvvvvvvvvvvv                          //
+	//                               vvvv                               //
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"         // ahsai tts
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ai_false"      // 服务器监控
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aipaint"       // ai绘图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"        // 随机老婆
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/alipayvoice"   // 支付宝到账语音
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/b14"           // base16384加解密
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/baidu"         // 百度一下
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/baiduaudit"    // 百度内容审核
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/base64gua"     // base64卦加解密
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/baseamasiro"   // base天城文加解密
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili"      // b站相关
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/book_review"   // 哀伤雪刃吧推书记录
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/cangtoushi"    // 藏头诗
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/choose"        // 选择困难症帮手
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chouxianghua"  // 说抽象话
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chrev"         // 英文字符翻转
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/coser"         // 三次元小姐姐
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/cpstory"       // cp短打
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/danbooru"      // DeepDanbooru二次元图标签识别
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/diana"         // 嘉心糖发病
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/dress"         // 女装
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/drift_bottle"  // 漂流瓶
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/emojimix"      // 合成emoji
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/event"         // 好友申请群聊邀请事件处理
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/font"          // 渲染任意文字到图片
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/fortune"       // 运势
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/funny"         // 笑话
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/genshin"       // 原神抽卡
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/gif"           // 制图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/github"        // 搜索GitHub仓库
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/guessmusic"    // 猜歌
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/heisi"         // 黑丝
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/hs"            // 炉石
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/hyaku"         // 百人一首
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/image_finder"  // 关键字搜图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/inject"        // 注入指令
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jandan"        // 煎蛋网无聊图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jiami"         // 兽语加密
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jikipedia"     // 小鸡词典
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jptingroom"    // 日语听力学习材料
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/juejuezi"      // 绝绝子生成器
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/lolicon"       // lolicon 随机图片
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/magicprompt"   // magicprompt吟唱提示
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/midicreate"    // 简易midi音乐制作
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/moegoe"        // 日韩 VITS 模型拟声
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/moyu"          // 摸鱼
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/moyu_calendar" // 摸鱼人日历
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/music"         // 点歌
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/nativesetu"    // 本地涩图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/nativewife"    // 本地老婆
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/nbnhhsh"       // 拼音首字母缩写释义工具
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/nihongo"       // 日语语法学习
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/novel"         // 铅笔小说网搜索
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/nsfw"          // nsfw图片识别
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/omikuji"       // 浅草寺求签
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/qqwife"        // 一群一天一夫一妻制群老婆
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/quan"          // QQ权重查询
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/qzone"         // qq空间表白墙
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/realcugan"     // realcugan清晰术
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/reborn"        // 投胎
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/runcode"       // 在线运行代码
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/saucenao"      // 以图搜图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/scale"         // 叔叔的AI二次元图片放大
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/score"         // 分数
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/setutime"      // 来份涩图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/shadiao"       // 沙雕app
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/shindan"       // 测定
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tarot"         // 抽塔罗牌
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tiangou"       // 舔狗日记
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tracemoe"      // 搜番
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/translation"   // 翻译
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/vitsnyaru"     // vits猫雷
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/vtb_quotation" // vtb语录
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wallet"        // 钱包
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wangyiyun"     // 网易云音乐热评
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wenben"        // 文本指令大全
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wenxinAI"      // 百度文心AI画图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/word_count"    // 聊天热词
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordle"        // 猜单词
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygo"           // 游戏王相关插件
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ymgal"         // 月幕galgame
+
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wtf"            // 鬼东西
+
+	//                               ^^^^                               //
+	//                          ^^^^^^^^^^^^^^                          //
+	//                      ^^^^^^^中优先级区^^^^^^^                      //
+	//               ^^^^^^^^^^^^^^中优先级区^^^^^^^^^^^^^^               //
+	// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^中优先级区^^^^^^^^^^^^^^^^^^^^^^^^^^^^ //
+	// ----------------------------中优先级区---------------------------- //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	// ----------------------------低优先级区---------------------------- //
+	// vvvvvvvvvvvvvvvvvvvvvvvvvvvv低优先级区vvvvvvvvvvvvvvvvvvvvvvvvvvvv //
+	//               vvvvvvvvvvvvvv低优先级区vvvvvvvvvvvvvv               //
+	//                      vvvvvvv低优先级区vvvvvvv                      //
+	//                          vvvvvvvvvvvvvv                          //
+	//                               vvvv                               //
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/curse" // 骂人
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ai_reply" // 人工智能回复
+
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/breakrepeat" // 打断复读
+
+	//                               ^^^^                               //
+	//                          ^^^^^^^^^^^^^^                          //
+	//                      ^^^^^^^低优先级区^^^^^^^                      //
+	//               ^^^^^^^^^^^^^^低优先级区^^^^^^^^^^^^^^               //
+	// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^低优先级区^^^^^^^^^^^^^^^^^^^^^^^^^^^^ //
+	// ----------------------------低优先级区---------------------------- //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	//                                                                  //
+	// -----------------------以下为内置依赖，勿动------------------------ //
+	"github.com/FloatTech/floatbox/process"
+	"github.com/sirupsen/logrus"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/driver"
+	"github.com/wdvxdr1123/ZeroBot/message"
+	// -----------------------以上为内置依赖，勿动------------------------ //
+)
+
+type zbpcfg struct {
+	Z zero.Config        `json:"zero"`
+	W []*driver.WSClient `json:"ws"`
+	S []*driver.WSServer `json:"wss"`
+}
+
+var config zbpcfg
+
+func init() {
+	sus := make([]int64, 0, 16)
+	// 解析命令行参数
+	d := flag.Bool("d", false, "Enable debug level log and higher.")
+	w := flag.Bool("w", false, "Enable warning level log and higher.")
+	h := flag.Bool("h", false, "Display this help.")
+	// 直接写死 AccessToken 时，请更改下面第二个参数
+	token := flag.String("t", "", "Set AccessToken of WSClient.")
+	// 直接写死 URL 时，请更改下面第二个参数
+	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
+	// 默认昵称
+	adana := flag.String("n", "C1enBot", "Set default nickname.")
+	prefix := flag.String("p", "/", "Set command prefix.")
+	runcfg := flag.String("c", "", "Run from config file.")
+	save := flag.String("s", "", "Save default config to file and exit.")
+	late := flag.Uint("l", 233, "Response latency (ms).")
+	rsz := flag.Uint("r", 4096, "Receiving buffer ring size.")
+	maxpt := flag.Uint("x", 4, "Max process time (min).")
+
+	flag.Parse()
+
+	if *h {
+		fmt.Println("Usage:")
+		flag.PrintDefaults()
+		os.Exit(0)
+	} else {
+		if *d && !*w {
+			logrus.SetLevel(logrus.DebugLevel)
+		}
+		if *w {
+			logrus.SetLevel(logrus.WarnLevel)
+		}
+	}
+
+	for _, s := range flag.Args() {
+		i, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			continue
+		}
+		sus = append(sus, i)
+	}
+
+	// 通过代码写死的方式添加主人账号
+	sus = append(sus, 1182016524)
+	// sus = append(sus, 87654321)
+
+	if *runcfg != "" {
+		f, err := os.Open(*runcfg)
+		if err != nil {
+			panic(err)
+		}
+		config.W = make([]*driver.WSClient, 0, 2)
+		err = json.NewDecoder(f).Decode(&config)
+		f.Close()
+		if err != nil {
+			panic(err)
+		}
+		config.Z.Driver = make([]zero.Driver, len(config.W)+len(config.S))
+		for i, w := range config.W {
+			config.Z.Driver[i] = w
+		}
+		for i, s := range config.S {
+			config.Z.Driver[i+len(config.W)] = s
+		}
+		logrus.Infoln("[main] 从", *runcfg, "读取配置文件")
+		return
+	}
+
+	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
+	config.Z = zero.Config{
+		NickName:       append([]string{*adana}, "C1enBot", "CB"),
+		CommandPrefix:  *prefix,
+		SuperUsers:     sus,
+		RingLen:        *rsz,
+		Latency:        time.Duration(*late) * time.Millisecond,
+		MaxProcessTime: time.Duration(*maxpt) * time.Minute,
+		Driver:         []zero.Driver{config.W[0]},
+	}
+
+	if *save != "" {
+		f, err := os.Create(*save)
+		if err != nil {
+			panic(err)
+		}
+		err = json.NewEncoder(f).Encode(&config)
+		f.Close()
+		if err != nil {
+			panic(err)
+		}
+		logrus.Infoln("[main] 配置文件已保存到", *save)
+		os.Exit(0)
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano()) // 全局 seed，其他插件无需再 seed
+	// 帮助
+	zero.OnFullMatchGroup([]string{"/help", ".help", "菜单"}, zero.OnlyToMe).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			ctx.SendChain(message.Text(kanban.Banner, "\n可发送\"/服务列表\"查看 bot 功能"))
+		})
+	zero.OnFullMatch("查看zbp公告", zero.OnlyToMe, zero.AdminPermission).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			ctx.SendChain(message.Text(kanban.Kanban()))
+		})
+	zero.RunAndBlock(&config.Z, process.GlobalInitMutex.Unlock)
+}
