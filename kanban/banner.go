9efe4bf96cbdc4b796e07a82ba555e5d68fd0b49
ChenHaoyang <63438478+C1en-QwQ@users.noreply.github.com>
2023-01-09 07:11:25 +0800

    Add files via upload

---
+++ kanban/banner.go
@@ -0,0 +1,7 @@
+package kanban
+
+// Banner ...
+var Banner = "* OneBot + ZeroBot + Golang + C1enBot\n" +
+	"* Version v1.6.1-beta3 - 2023-1-8 13:45:09 +0800 CST\n" +
+	"* Copyright © 2020 - 2023 C1enDev. All Rights Reserved.\n" +
+	"* Project: https://github.com/C1enDev/C1enBot"
---
+++ kanban/init.go
@@ -0,0 +1,45 @@
+// Package kanban 打印版本信息
+package kanban
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/FloatTech/zbputils/control"
+	"github.com/fumiama/go-registry"
+)
+
+//go:generate go run github.com/FloatTech/ZeroBot-Plugin/kanban/gen
+
+var once sync.Once
+
+func init() {
+	once.Do(PrintBanner)
+}
+
+var reg = registry.NewRegReader("reilia.fumiama.top:32664", control.Md5File, "fumiama")
+
+// PrintBanner ...
+func PrintBanner() {
+	fmt.Print(
+		"\n======================[C1enBot-Plugin]======================",
+		"\n", Banner, "\n",
+		"----------------------[C1enBot-公告栏]----------------------",
+		"\n", Kanban(), "\n",
+		"============================================================\n\n",
+	)
+}
+
+// Kanban ...
+func Kanban() string {
+	err := reg.Connect()
+	if err != nil {
+		return err.Error()
+	}
+	defer reg.Close()
+	text, err := reg.Get("C1enBot/kanban")
+	if err != nil {
+		return err.Error()
+	}
+	return text
+}
