4b21789ddb2e0c67dcbe2caa76eba90642fdf0c4
ChenHaoyang <63438478+C1en-QwQ@users.noreply.github.com>
2023-01-09 08:08:26 +0800

    Update banner.go

--- kanban/banner.go
+++ kanban/banner.go
@@ -4,5 +4,5 @@ package kanban
 var Banner = "* OneBot + ZeroBot + Golang + C1enBot\n" +
 	"* Version v1.6.1-beta3 - 2023-1-8 13:45:09 +0800 CST\n" +
 	"* Copyright © 2020 - 2023 C1enDev. All Rights Reserved.\n" +
-	"* Project: https://github.com/C1enDev/C1enBot"
+	"* Project: https://github.com/C1enDev/C1enBot\n"
 	"* C1enDev-QQ-454102262"
